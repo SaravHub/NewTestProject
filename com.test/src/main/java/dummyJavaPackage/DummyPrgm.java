ac0ae35acb6d9c920b87357731a0d06cdfef7b3a
Sarav <Sarav@testI>
2021-10-12 19:15:58 +0530

    new println added through eclipse

--- com.test/src/main/java/dummyJavaPackage/DummyPrgm.java
+++ com.test/src/main/java/dummyJavaPackage/DummyPrgm.java
@@ -4,5 +4,6 @@ public class DummyPrgm {
 
 	public static void main(String[] args) {
 		System.out.println("hai this is dummy java prgm");
+		System.out.println("new line added in Dummy prgm");
 	}
 }
