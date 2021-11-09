98853b0e5b407e6db09a2131327c8cf6ef3896c7
Sarav <Sarav@testI>
2021-11-09 08:18:16 +0530

    DummyPrgm Added

--- com.test/src/main/java/dummyJavaPackage/DummyPrgm.java
+++ com.test/src/main/java/dummyJavaPackage/DummyPrgm.java
@@ -1,5 +1,9 @@
 package dummyJavaPackage;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class DummyPrgm {
 
 	public static void main(String[] args) {
@@ -7,5 +11,8 @@ public class DummyPrgm {
 		System.out.println("new line added in Dummy prgm");
 		//hi
 		System.out.println("new line added through Git Repo");
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
 	}
 }
